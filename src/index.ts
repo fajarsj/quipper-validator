39c87e1dd00ec0dd46ff59801f00a0fb094c13ed
fajarsj <fajarthea5@gmail.com>
2021-12-10 17:38:54 +0700

    add interfact to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import isEmail from "./lib/isEmail";
 import isEmpty, { Options as OptionsIsEmpty } from "./lib/isEmpty";
 import isUsername from "./lib/isUsername";
-import isPassword, { Options as OptionsIsPassword } from "./lib/isPassword";
+import isPassword, {
+  Options as OptionsIsPassword,
+  Error as ErrorIsPassword,
+} from "./lib/isPassword";
 
 interface Validator {
   isEmail: (str: string) => boolean;
   isEmpty: (str: string, options?: OptionsIsEmpty) => boolean;
   isUsername: (str: string) => boolean;
-  isPassword: (str: string, options?: OptionsIsPassword) => boolean;
+  isPassword: (
+    str: string,
+    options?: OptionsIsPassword
+  ) => boolean | ErrorIsPassword;
 }
 
 const validator: Validator = {
